nalldredge/owncloud-client

prototype of bundled requests in the client

src/libsync/owncloudpropagator.cpp
@@ -303,6 +303,10 @@ void OwncloudPropagator::start(const SyncFileItemVector& items)
     directories.push(qMakePair(QString(), _rootJob.data()));
     QVector<PropagatorJob*> directoriesToRemove;
     QString removedDirectory;
+
+    PropagateBundleQNAM *bundledRequestsJob = new PropagateBundleQNAM(this);
+    quint64 chunkSize = OwncloudPropagator::chunkSize();
+    bool bundledRequestsEnabled = _account->bundledRequestsEnabled();
     foreach(const SyncFileItemPtr &item, items) {
 
         if (!removedDirectory.isEmpty() && item->_file.startsWith(removedDirectory)) {
@@ -379,6 +383,12 @@ void OwncloudPropagator::start(const SyncFileItemVector& items)
                 currentDirJob->append(dir);
             }
             directories.push(qMakePair(item->destination() + "/" , dir));
+        } else if (bundledRequestsEnabled
+                  && (item->_instruction == CSYNC_INSTRUCTION_NEW)
+                  && (item->_direction == SyncFileItem::Up)
+                  && (item->_size < chunkSize)) {
+            //this will create list of bundle files to sync for that bundlejob
+            bundledRequestsJob->append(item);
         } else if (PropagateItemJob* current = createJob(item)) {
             if (item->_instruction == CSYNC_INSTRUCTION_TYPE_CHANGE) {
                 // will delete directories, so defer execution
@@ -389,6 +399,12 @@ void OwncloudPropagator::start(const SyncFileItemVector& items)
             }
         }
     }
+
+    //TODO Add support for more bundled jobs if bundleJob->append fails, please refer to class declaration
+    if (bundledRequestsEnabled && !bundledRequestsJob->empty()) {
+        _rootJob->append(bundledRequestsJob);
+    }
+
 
     foreach(PropagatorJob* it, directoriesToRemove) {
         _rootJob->append(it);

tmartos/client

src/libsync/owncloudpropagator.cpp
@@ -303,6 +303,10 @@ void OwncloudPropagator::start(const SyncFileItemVector& items)
     directories.push(qMakePair(QString(), _rootJob.data()));
     QVector<PropagatorJob*> directoriesToRemove;
     QString removedDirectory;
+
+    PropagateBundle *bundledRequestsJob = new PropagateBundle(this);
+    quint64 chunkSize = OwncloudPropagator::chunkSize();
+    bool bundledRequestsEnabled = _account->bundledRequestsEnabled();
     foreach(const SyncFileItemPtr &item, items) {
 
         if (!removedDirectory.isEmpty() && item->_file.startsWith(removedDirectory)) {
@@ -379,6 +383,12 @@ void OwncloudPropagator::start(const SyncFileItemVector& items)
                 currentDirJob->append(dir);
             }
             directories.push(qMakePair(item->destination() + "/" , dir));
+        } else if (bundledRequestsEnabled
+                  && (item->_instruction == CSYNC_INSTRUCTION_NEW)
+                  && (item->_direction == SyncFileItem::Up)
+                  && (item->_size < chunkSize)) {
+            //this will create list of bundle files to sync for that bundlejob
+            bundledRequestsJob->append(item);
         } else if (PropagateItemJob* current = createJob(item)) {
             if (item->_instruction == CSYNC_INSTRUCTION_TYPE_CHANGE) {
                 // will delete directories, so defer execution
@@ -389,6 +399,15 @@ void OwncloudPropagator::start(const SyncFileItemVector& items)
             }
         }
     }
+
+    //TODO Add support for more bundled jobs if bundleJob->append fails, please refer to class declaration
+    if (bundledRequestsEnabled && !bundledRequestsJob->empty()) {
+        _rootJob->append(bundledRequestsJob);
+    }
+    else{
+        delete bundledRequestsJob;
+    }
+
 
     foreach(PropagatorJob* it, directoriesToRemove) {
         _rootJob->append(it);